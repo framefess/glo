757fb9707cc4146f9e559fc8ec98c8c04963bc7c
framefess <framefess@gmail.com>
2020-01-13 01:17:56 +0700

    home

--- src/home/home.php
+++ src/home/home.php
@@ -3,18 +3,33 @@
 <body class="bg-light">
     <div class="container mt-3">
         <div class="row">
-            <div class="col text-left">
+            <div class="col-3 text-left">
                 <h1>ผลโหวตล่าสุด</h1>
             </div>
+            <div class="col-4 text-left align-self-center">
+                
+                <button type="button" class="btn btn-dark" style="position: inline;">ทีมที่เข้าร่วม</button>
+                <span class="badge badge-danger pull-right">โหวตเลย</span>
+            </div>
+            <div class="col text-right">
+                <h1 class="w-100 position-absolute my-auto text-right fixed-top "><button type="button" class="btn" style="width: 12%"><img src="assets/system.svg" /></button></h1>
+            </div>
         </div>
-        <div class="row  bar1">
-            <div class="col-auto text-left text-light font-weight-bold" style="width: 52.90%;">ชื่อทีมที่ 1</div>
-            <div class="col-auto text-right text-dark" style="width: 40%;">
+        <div class="row  bar1 shadow-sm mb-1 rounded align-items-center">
+            <div class="col-auto text-left text-light font-weight-bold pt-1 pb-1" style="width: 52.50%;">
+                <div class="row align-items-center">
+                    <div class="col-auto">
+                        <h1 class="d-inline">1 <img src="http://via.placeholder.com/50x50" style="width: 50px; height: 50px" /></h1>
+                        <h3 class="d-inline"> ชื่อทีม</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="col-auto text-right text-dark align-self-center" style="width: 40%;">
                 <div class="row justify-content-md-center">
                     <div class="col-auto text-right text-dark font-weight-bold">คะแนนโหวต : </div>
                     <div class="col-auto text-left text-dark font-weight-bold">100</div>
-                    <div class="col-auto align-self-center" style="width: 30%">
-                        <div class="progress" style="width: 100%">
+                    <div class="col-auto align-self-center" style="width: 100px">
+                        <div class="progress" style="width: 100px">
                             <div class="progress-bar" role="progressbar" style="width: 20%;" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">20%</div>
                         </div>
                     </div>
@@ -22,56 +37,84 @@
             </div>
 
         </div>
-        <div class="row ml-3 bar2">
-            <div class="col-auto text-left text-light font-weight-bold" style="width: 52%;">ชื่อทีมที่ 2</div>
-            <div class="col-auto text-right text-dark" style="width: 40%;">
+        <div class="row ml-3 bar2 shadow-sm mb-1 rounded">
+            <div class="col-auto text-left text-light font-weight-bold pt-1 pb-1" style="width: 51.75%;">
+                <div class="row align-items-center">
+                    <div class="col-auto">
+                        <h1 class="d-inline">2 <img src="http://via.placeholder.com/50x50" style="width: 50px; height: 50px" /></h1>
+                        <h3 class="d-inline"> ชื่อทีม</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="col-auto text-right text-dark align-self-center" style="width: 40%;">
                 <div class="row justify-content-md-center">
                     <div class="col-auto text-right text-dark font-weight-bold">คะแนนโหวต : </div>
                     <div class="col-auto text-left text-dark font-weight-bold">100</div>
-                    <div class="col-auto align-self-center" style="width: 30%">
-                        <div class="progress" style="width: 100%">
+                    <div class="col-auto align-self-center" style="width: 100px">
+                        <div class="progress" style="width: 100px">
                             <div class="progress-bar" role="progressbar" style="width: 20%;" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">20%</div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="row ml-5 bar3">
-            <div class="col-auto  text-left text-dark font-weight-bold" style="width: 51%;" >ชื่อทีมที่ 3</div>
-            <div class="col-auto text-right text-dark" style="width: 40%;">
+        <div class="row ml-5 bar3 shadow-sm mb-1 rounded">
+            <div class="col-auto  text-left text-dark font-weight-bold pt-1 pb-1" style="width: 50.90%;">
+                <div class="row align-items-center">
+                    <div class="col-auto">
+                        <h1 class="d-inline">3 <img src="http://via.placeholder.com/50x50" style="width: 50px; height: 50px" /></h1>
+                        <h3 class="d-inline"> ชื่อทีม</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="col-auto text-right text-dark align-self-center" style="width: 40%;">
                 <div class="row justify-content-md-center">
                     <div class="col-auto text-right text-dark font-weight-bold">คะแนนโหวต : </div>
                     <div class="col-auto text-left text-dark font-weight-bold">100</div>
-                    <div class="col-auto align-self-center" style="width: 30%">
-                        <div class="progress" style="width: 100%">
+                    <div class="col-auto align-self-center" style="width: 100px">
+                        <div class="progress" style="width: 100px">
                             <div class="progress-bar" role="progressbar" style="width: 20%;" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">20%</div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="row ml-7 bar4">
-            <div class="col-auto  text-left font-weight-bold" style="width: 50%;">ชื่อทีมที่ 4</div>
-            <div class="col-auto text-right text-dark" style="width: 40%;">
+        <div class="row ml-7 bar4 shadow-sm mb-1 rounded">
+            <div class="col-auto  text-left font-weight-bold pt-1 pb-1" style="width: 50%;">
+                <div class="row align-items-center">
+                    <div class="col-auto">
+                        <h1 class="d-inline">4 <img src="http://via.placeholder.com/50x50" style="width: 50px; height: 50px" /></h1>
+                        <h3 class="d-inline"> ชื่อทีม</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="col-auto text-right text-dark align-self-center" style="width: 40%;">
                 <div class="row justify-content-md-center">
                     <div class="col-auto text-right text-dark font-weight-bold">คะแนนโหวต : </div>
                     <div class="col-auto text-left text-dark font-weight-bold">100</div>
-                    <div class="col-auto align-self-center" style="width: 30%">
-                        <div class="progress" style="width: 100%">
+                    <div class="col-auto align-self-center" style="width: 100px">
+                        <div class="progress" style="width: 100px">
                             <div class="progress-bar" role="progressbar" style="width: 20%;" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">20%</div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="row ml-7 bar4">
-            <div class="col-auto text-left font-weight-bold" style="width: 50%;">ชื่อทีมที่ 5</div>
-            <div class="col-auto text-right text-dark" style="width: 40%;">
+        <div class="row ml-7 bar4 shadow-sm rounded">
+            <div class="col-auto text-left font-weight-bold pt-1 pb-1" style="width: 50%;">
+                <div class="row align-items-center">
+                    <div class="col-auto">
+                        <h1 class="d-inline">5 <img src="http://via.placeholder.com/50x50" style="width: 50px; height: 50px" /></h1>
+                        <h3 class="d-inline"> ชื่อทีม</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="col-auto text-right text-dark align-self-center " style="width: 40%;">
                 <div class="row justify-content-md-center">
                     <div class="col-auto text-right text-dark font-weight-bold">คะแนนโหวต : </div>
                     <div class="col-auto text-left text-dark font-weight-bold">100</div>
-                    <div class="col-auto align-self-center" style="width: 30%">
-                        <div class="progress" style="width: 100%">
+                    <div class="col-auto align-self-center" style="width: 100px">
+                        <div class="progress" style="width: 100px">
                             <div class="progress-bar" role="progressbar" style="width: 20%;" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">20%</div>
                         </div>
                     </div>

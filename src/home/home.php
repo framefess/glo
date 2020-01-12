94fe5d10481096b2b2de245583e5a84c0d657fbe
framefess <framefess@gmail.com>
2020-01-12 22:20:33 +0700

    poll vote

--- src/home/home.php
+++ src/home/home.php
@@ -1,23 +1,82 @@
 <link rel="stylesheet" href="src/home/home.css">
-<body class="">
-    <div class="container">
-        <div class="row ">
-            <div class="col text-left"><h1>ผลโหวตล่าสุด</h1></div>
+
+<body class="bg-light">
+    <div class="container mt-3">
+        <div class="row">
+            <div class="col text-left">
+                <h1>ผลโหวตล่าสุด</h1>
+            </div>
         </div>
         <div class="row  bar1">
-            <div class="col text-left">test</div>
+            <div class="col-auto text-left text-light font-weight-bold" style="width: 52.90%;">ชื่อทีมที่ 1</div>
+            <div class="col-auto text-right text-dark" style="width: 40%;">
+                <div class="row justify-content-md-center">
+                    <div class="col-auto text-right text-dark font-weight-bold">คะแนนโหวต : </div>
+                    <div class="col-auto text-left text-dark font-weight-bold">100</div>
+                    <div class="col-auto align-self-center" style="width: 30%">
+                        <div class="progress" style="width: 100%">
+                            <div class="progress-bar" role="progressbar" style="width: 20%;" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">20%</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
         <div class="row ml-3 bar2">
-            <div class="col text-left">test</div>
+            <div class="col-auto text-left text-light font-weight-bold" style="width: 52%;">ชื่อทีมที่ 2</div>
+            <div class="col-auto text-right text-dark" style="width: 40%;">
+                <div class="row justify-content-md-center">
+                    <div class="col-auto text-right text-dark font-weight-bold">คะแนนโหวต : </div>
+                    <div class="col-auto text-left text-dark font-weight-bold">100</div>
+                    <div class="col-auto align-self-center" style="width: 30%">
+                        <div class="progress" style="width: 100%">
+                            <div class="progress-bar" role="progressbar" style="width: 20%;" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">20%</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
         <div class="row ml-5 bar3">
-            <div class="col  text-left">test</div>
+            <div class="col-auto  text-left text-dark font-weight-bold" style="width: 51%;" >ชื่อทีมที่ 3</div>
+            <div class="col-auto text-right text-dark" style="width: 40%;">
+                <div class="row justify-content-md-center">
+                    <div class="col-auto text-right text-dark font-weight-bold">คะแนนโหวต : </div>
+                    <div class="col-auto text-left text-dark font-weight-bold">100</div>
+                    <div class="col-auto align-self-center" style="width: 30%">
+                        <div class="progress" style="width: 100%">
+                            <div class="progress-bar" role="progressbar" style="width: 20%;" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">20%</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="row ml-6 bar4">
-            <div class="col text-left">test</div>
+        <div class="row ml-7 bar4">
+            <div class="col-auto  text-left font-weight-bold" style="width: 50%;">ชื่อทีมที่ 4</div>
+            <div class="col-auto text-right text-dark" style="width: 40%;">
+                <div class="row justify-content-md-center">
+                    <div class="col-auto text-right text-dark font-weight-bold">คะแนนโหวต : </div>
+                    <div class="col-auto text-left text-dark font-weight-bold">100</div>
+                    <div class="col-auto align-self-center" style="width: 30%">
+                        <div class="progress" style="width: 100%">
+                            <div class="progress-bar" role="progressbar" style="width: 20%;" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">20%</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="row ml-5 bar4">
-            <div class="col text-left">test</div>
+        <div class="row ml-7 bar4">
+            <div class="col-auto text-left font-weight-bold" style="width: 50%;">ชื่อทีมที่ 5</div>
+            <div class="col-auto text-right text-dark" style="width: 40%;">
+                <div class="row justify-content-md-center">
+                    <div class="col-auto text-right text-dark font-weight-bold">คะแนนโหวต : </div>
+                    <div class="col-auto text-left text-dark font-weight-bold">100</div>
+                    <div class="col-auto align-self-center" style="width: 30%">
+                        <div class="progress" style="width: 100%">
+                            <div class="progress-bar" role="progressbar" style="width: 20%;" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">20%</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
     <?php include "src/module/module.php"; ?>

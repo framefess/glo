8d902c0dfd593af2452cb617f1f66c2a6ecfefd5
framefess <framefess@gmail.com>
2020-01-12 20:27:46 +0700

    ๅ

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 include "src/header/header.php";
-include "src/signup/signup.php";
+// include "src/signup/signup.php";
+
+include "src/home/home.php";
 ?>
 
---
+++ src/home/home.php
@@ -0,0 +1,26 @@
+<link rel="stylesheet" href="src/home/home.css">
+<body class="">
+    <div class="container">
+        <div class="row ">
+            <div class="col text-left"><h1>ผลโหวตล่าสุด</h1></div>
+        </div>
+        <div class="row  bar1">
+            <div class="col text-left">test</div>
+        </div>
+        <div class="row ml-3 bar2">
+            <div class="col text-left">test</div>
+        </div>
+        <div class="row ml-5 bar3">
+            <div class="col  text-left">test</div>
+        </div>
+        <div class="row ml-6 bar4">
+            <div class="col text-left">test</div>
+        </div>
+        <div class="row ml-5 bar4">
+            <div class="col text-left">test</div>
+        </div>
+    </div>
+    <?php include "src/module/module.php"; ?>
+</body>
+
+</html>

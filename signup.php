ea801e01c60752999656d8cd36f28e7652afe432
framefess <framefess@gmail.com>
2020-01-14 18:58:14 +0700

    deploy

---
+++ signup.php
@@ -0,0 +1,189 @@
+<style>
+  body {
+    /* background: rgb(34, 186, 195);
+    background: linear-gradient(0deg, rgba(34, 186, 195, 1) 0%, rgba(2, 12, 255, 1) 100%) no-repeat center center fixed;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover; */
+    background: rgb(0, 136, 207);
+    background: -moz-linear-gradient(0deg, rgba(0, 136, 207, 1) 51%, rgba(2, 12, 255, 1) 100%);
+    background: -webkit-linear-gradient(0deg, rgba(0, 136, 207, 1) 51%, rgba(2, 12, 255, 1) 100%);
+    background: linear-gradient(0deg, rgba(0, 136, 207, 1) 51%, rgba(2, 12, 255, 1) 100%) no-repeat center center fixed;
+    filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#0088cf", endColorstr="#020cff", GradientType=1);
+  }
+</style>
+
+<body class="">
+  <div class="mt-3 container" style="width: 900px; height: 600px;">
+    <div id="section1"></div>
+    <section class="row no-gutter align-items-center">
+      <div class="col-lg-12 text-center p-0 d-flex align-items-center">
+        <img class="img-fluid position-relative mx-auto" src="assets/poster/poster.jpg" width="900px" height="600px" />
+        <!-- <h1 class="w-100 position-absolute my-auto text-right fixed-top ">
+          <div class="btn-group">
+            <button type="button" class="btn" onclick="location.href='index.php?home'"><i class="fas fa-home text-primary" style="width: 44px;height: 44px;"></i></button>
+            <button type="button" class="btn" style="width: 77px" onclick="location.href='login.php'"><img src="assets/system.svg" /></button>
+          </div>
+        </h1> -->
+        <h3 class="w-100 position-absolute my-auto text-right fixed-bottom ">
+          <button type="button" id="bdesc" class="btn btn-info btn-lg float-right" data-toggle="collapse" data-target="#description" aria-expanded="false" aria-controls="description">เงื่อนไข</button>
+        </h3>
+      </div>
+    </section>
+    <div id="section2"></div>
+    <div class="collapse bg-info" id="description">
+      <div class="row bg-info">
+        <div class="col card card-body text-left bg-info">
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item list-group-item-light">
+              <h1 class="text-info">คุณสมบัติ</h1>
+              <ul class="">
+                <li class="">รวมทีม ทีมละ 4 ท่าน ไม่จำกัด เพศ และ อายุ สังกัดหน่วยงานเดียวกันหรือต่างหน่วยงานก็ได้ โดยใน 1 ทีม ต้องมีสมาชิกทั้งเพศชายและหญิง</li>
+                <li class="">สมาชิกในทีมทั้งหมดต้องเป็นบุคลากรในสังกัดสำนักงานสลากกินแบ่งรัฐบาล</li>
+                <li class="">มีสุขภาพที่สมบูรณ์ทั้งร่างกายและจิตใจสามารถร่วมกิจกรรมได้ตลอดโครงการฯ</li>
+                <li class="">มีความรู้ มีความมั่นใจ ทันสมัย</li>
+                <li class="">กล้าแสดงออก กล้าคิด กล้าทำ</li>
+              </ul>
+            </li>
+            <li class="list-group-item list-group-item-light">
+              <h1 class="text-info">หลักเกณฑ์การประกวด</h1>
+              <p>รอบการประกวดแบ่งเป็น 3 รอบ</p>
+              <p class="font-weight-bolder"><u>รอบที่ 1 รอบคัดเลือก 15 ทีม</u></p>
+              <p class="font-weight-bolder">พิจารณาจากเอกสารการสมัครและคลิปวิดีโอ</p>
+              <p>โดยคลิปวิดีโอคณะกรรมการจะพิจารณาจากบุคลิกภาพและการนำเสนอ <strong>ให้สอดคล้องกับ GLO Next Chapter เป็นผู้ให้ ทันสมัย โปร่งใส มืออาชีพ</strong> (ประกาศผลผู้ที่ผ่านเข้าสู่รอบ 15 ทีมสุดท้าย วันที่ 3 กุมภาพันธ์ 2563)</p>
+              <p>การตัดสิน รวม 100 คะแนน ของคณะกรรมการยึดถือหลักเกณฑ์ ดังนี้</p>
+              <ul class="">
+                <li class="">แนวคิด/Concept ของทีม 30 คะแนน</li>
+                <li class="">ความคิดสร้างสรรค์และการนำเสนอ 30 คะแนน</li>
+                <li class="">การให้ความสำคัญกับบทบาทกับทุกคนในทีม 30 คะแนน</li>
+                <li class="">ความเหมาะสมของเครื่องแต่งกาย 10 คะแนน</li>
+              </ul>
+              <p class="font-weight-bolder"><u>รอบที่ 2 รอบคัดเลือก 5 ทีม</u></p>
+              <p class="font-weight-bolder">พิจารณาจากเอกสารการสมัครและคลิปวิดีโอ</p>
+              <p>โดยคลิปวิดีโอคณะกรรมการจะพิจารณาจากบุคลิกภาพและการนำเสนอ <strong>ให้สอดคล้องกับ GLO Next Chapter เป็นผู้ให้ ทันสมัย โปร่งใส มืออาชีพ</strong> (ประกาศผลผู้ที่ผ่านเข้าสู่รอบ 5 ทีมสุดท้าย วันที่ 7 กุมภาพันธ์ 2563)</p>
+              <p>การตัดสิน รวม 100 คะแนน ของคณะกรรมการยึดถือหลักเกณฑ์ ดังนี้</p>
+              <ul class="pb-2">
+                <li class="">แนวคิด/Concept ของทีม 30 คะแนน</li>
+                <li class="">ความคิดสร้างสรรค์และการนำเสนอ 30 คะแนน</li>
+                <li class="">การให้ความสำคัญกับบทบาทกับทุกคนในทีม 30 คะแนน</li>
+                <li class="">ความเหมาะสมของเครื่องแต่งกาย 10 คะแนน</li>
+              </ul>
+              <p>ในรอบคัดเลือกจะเปิดให้บุคลากรของสำนักงานสลากกินแบ่งรัฐบาล ได้ร่วมโหวตทีมที่ชื่นชอบ ในวันที่ 4-6 กุมภาพันธ์ 2563 </p>
+              <p class="font-weight-bolder"><u>รอบที่ 3 รอบตัดสิน</u></p>
+              <p class="font-weight-bolder mb-0">วันที่ 12 กุมภาพันธ์ 2563 ผู้ประกวดต้องนำเสนอผลงานให้สอดคล้องกับ “GLO Next Chapter เป็นผู้ให้ ทันสมัย โปร่งใส มืออาชีพ” ในรูปแบบที่น่าสนใจ และตอบคำถามกรรมการ </p>
+              <p class="font-weight-bolder" style="color: rgb(255, 102, 0)">(วันที่ 10 กุมภาพันธ์ 2563 ผู้เข้ารอบตัดสินต้องเข้าร่วม Workshop เพื่อฝึกอบรมทักษะพัฒนาบุคลิกภาพโดยวิทยากรมืออาชีพ จาก สถาบันพัฒนาบุคลิกภาพ John Robert Power ก่อนวันแข่งขันรอบตัดสิน)</p>
+              <p>ในรอบตัดสินจะเปิดให้บุคลากรของสำนักงานสลากกินแบ่งรัฐบาล ได้ร่วมโหวตทีมที่ชื่นชอบ ในวันที่ 12-14 กุมภาพันธ์ 2563 เวลา 12.00 น.</p>
+              <p>การตัดสิน รวม 100 คะแนน ยึดถือหลักเกณฑ์ ดังนี้</p>
+              <ul class="">
+                <li class="">แนวคิด/Concept ของทีม 20 คะแนน</li>
+                <li class="">ความคิดสร้างสรรค์และการนำเสนอ 20 คะแนน</li>
+                <li class="">เนื้อหาของผลงานง่ายต่อการเข้าใจ 20 คะแนน</li>
+                <li class="">ความเป็นไปได้ในการนำไปปฏิบัติ/ใช้งานจริง 20 คะแนน</li>
+                <li class="">บุคลิกภาพในการเป็น Brand Ambassador 20 คะแนน</li>
+              </ul>
+            </li>
+            <li class="list-group-item list-group-item-light">
+              <h1 class="text-info">เอกสารประกอบการสมัคร</h1>
+              <ul class="">
+                <li class="">ใบสมัคร</li>
+                <li class="">รูปถ่ายรวมของทีม 1 ภาพ </li>
+                <li class="">คลิปวีดีโอนำเสนอแนวความคิด ในหัวข้อ <strong>“ GLO The Next Chapter 2020 เป็นผู้ให้ที่ทันสมัย โปร่งใส เป็นมืออาชีพ ”</strong> ความยาวไม่เกิน 3 นาที
+                  * ต้องอัพโหลดวีดีโอลงบนเว็บไซต์ยูทูป
+                </li>
+              </ul>
+            </li>
+            <li class="list-group-item list-group-item-light">
+              <h1 class="text-info">กำหนดการรับสมัคร</h1>
+              <ul class="">
+                <li class="">ตั้งแต่วันนี้ จนถึงวันที่ 30 มกราคม 2563</li>
+              </ul>
+            </li>
+            <li class="list-group-item list-group-item-light">
+              <h1 class="text-info">วิธีการสมัคร</h1>
+              <p class="mb-0">สมัครผ่านทางออนไลน์ โดยส่งใบสมัคร ภาพถ่ายทีม และคลิปวีดีโอ ผ่านทาง <a href="mailto:glonextchapter2020@gmail.com" target="_blank">glonextchapter2020@gmail.com</a>
+                </br>*คลิปวีดีโอต้อง <font style="color: rgb(255, 0, 0)">“อัพโหลดคลิปวีดีโอลงบนเว็บไซต์ยูทูปเท่านั้น”</font> และนำลิ้งก์ของวีดีโอแนบใส่ใน อีเมล
+              </p>
+              <ul class="list-group ">
+                <li class="list-group-item list-group-item-light">ดาวน์โหลดใบสมัคร :
+                  <a href='downloadfile.php?file=<?php echo urlencode("เงื่อนไขรับสมัคร 14 Jan.pdf"); ?>'>pdf</a>
+                  <a href='downloadfile.php?file=<?php echo urlencode("เงื่อนไขรับสมัคร 14 Jan.docx"); ?>'>doc</a>
+                </li>
+                <!-- <li class="list-group-item list-group-item-info">ใบสมัคร : <a href='assets/file/เงื่อนไขรับสมัคร 14 Jan.pdf'>ดาวโหลด</a></li> -->
+              </ul>
+              <p><strong>ติดต่อสอบถามรายละเอียด</strong></br>
+                โทร. 02-713-0198-99 ต่อ 100 (คุณรมิดา)
+              </p>
+            </li>
+            <li class="list-group-item list-group-item-light">
+              <h1 class="text-info">กำหนดการประกวด</h1>
+              <ul class="">
+                <li class="row">
+                  <p class="col-3 text-left"><strong>16 – 30 มกราคม 2563</strong></p>
+                  <p class="col-9 ">รับสมัครโดยส่งใบสมัคร ผ่านทาง glonextchapter2020@gmail.com</p>
+                </li>
+                <li class="row">
+                  <p class="col-3 text-left"><strong>3 กุมภาพันธ์ 2563</strong></p>
+                  <p class="col-9">ประกาศรายชื่อทีมที่ผ่านเข้ารอบ 15 ทีม ผ่านทาง <a href="http://www.glothenextchapter.com">www.glothenextchapter.com</a> </br>(เวลา 15.๐๐น.)</p>
+                </li>
+                <li class="row">
+                  <p class="col-3 text-left"><strong>7 กุมภาพันธ์ 2563</strong></p>
+                  <p class="col-9">ประกาศรายชื่อทีมที่ผ่านเข้ารอบ 5 ทีม ผ่านทาง <a href="http://www.glothenextchapter.com">www.glothenextchapter.com</a> </br>(เวลา 15.๐๐น.)</p>
+                </li>
+                <li class="row">
+                  <p class="col-3 text-left"><strong>12 กุมภาพันธ์ 2563 </strong></p>
+                  <p class="col-9">การประกวดรอบตัดสิน ผู้สมัครจะต้องนำเสนอผลงานและตอบคำถามคณะกรรมการในหัวข้อ GLO Next Chapter</p>
+                </li>
+                <li class="row">
+                  <p class="col-3 text-left"><strong>14 กุมภาพันธ์ 2563 </strong></p>
+                  <p class="col-9">ประกาศรายชื่อผู้ชนะผ่านทาง <a href="http://www.glothenextchapter.com">www.glothenextchapter.com</a> (เวลา 15.๐๐น.)</p>
+                </li>
+              </ul>
+            </li>
+
+            <li class="list-group-item list-group-item-light">
+              <h1 class="text-info">รางวัล</h1>
+              <p><strong>ผู้ได้รับตำแหน่งชนะเลิศ</strong> (จำนวน 1 รางวัล)</p>
+              <p class="mb-0"><u>สิ่งที่ผู้เข้าประกวดจะได้รับ</u></p>
+              <p class="ml-2">1. เงินสด มูลค่า 100,000 บาท</p>
+              <p><strong>ผู้ได้รับตำแหน่งรองชนะเลิศอันดับ ๑</strong> (จำนวน 4 รางวัล)</p>
+              <p class="mb-0"><u>สิ่งที่ผู้เข้าประกวดจะได้รับ</u></p>
+              <p class="ml-2">1. เงินสด มูลค่า 10,000 บาท</p>
+            </li>
+
+            <li class="list-group-item list-group-item-light">
+              <h1 class="text-info">หน้าที่ของ GLO BRAND AMBASSADOR 2020</h1>
+              <p class="ml-2">
+                1. เป็นตัวแทนขององค์กร ในการนำเสนอภาพลักษณ์ในการทำงานแบบ “ เป็นผู้ให้ที่ทันสมัย โปร่งใส เป็นมืออาชีพ” </br>
+                2. ช่วยในการผลักดันให้องค์กร ก้าวเข้าสู่การเป็นผู้ให้ที่ ทันสมัย โปร่งใส เป็นมืออาชีพ</br>
+                3. เป็นตัวแทนหลักในการเข้าร่วมกิจกรรมทุกกิจกรรม ที่เกี่ยวข้องกับการนำเสนอภาพลักษณ์ในการทำงาน ตามที่องค์กรเห็นสมควร
+              </p>
+            </li>
+
+            <li class="list-group-item list-group-item-light">
+              <h1 class="text-info">หมายเหตุ</h1>
+              <ul>
+                <li>ขณะทำการประกวด หากเกิดข้อผิดพลาดจากซาวด์หรือภาพของผู้เข้าประกวดที่ส่ง ซึ่งไม่ได้เกิดจากเครื่องเสียง ทางคณะกรรมการจัดการประกวดจะไม่รับผิดชอบและต้องอยู่ในดุลพินิจของคณะกรรมการตัดสินซึ่งเป็นผู้ชี้ขาด</li>
+                <li>ช่วงเวลาในการโหวต บุคลากร 1 ท่าน จะมีเพียง 1 บัญชีรายชื่อและ 1 บัญชีรายชื่อ สามารถโหวตได้ 2 ครั้งต่อวัน คือช่วงเวลา 08.00-12.00น.และ 12.01-23.59 น. ช่วงเวลาละ 1 คะแนน</li>
+                <li>ค่าใช้จ่ายภาษี หัก ณ ที่จ่ายเป็นภาระของผู้ที่ได้รับรางวัล จะต้องชำระด้วยตัวเอง</li>
+                <li>คณะกรรมการสามารถตัดสิทธิ์และมีสิทธิ์เด็ดขาดในการรับรางวัล กรณีที่พบว่ามีการกระทำเข้าข่ายทุจริตหรือไม่เป็นไปตามข้อกำหนดทุกรูปแบบในการประกวดครั้งนี้</li>
+                <li>ทางบริษัทฯ ขอสงวนสิทธิ์ในการเปลี่ยนแปลงกฏเกณฑ์ของรางวัล, เงื่อนไขและกติกาโดยมิต้องแจ้งให้ทราบล่วงหน้า</li>
+                <li>ผลจากการตัดสินของคณะกรรมการถือเป็นที่สิ้นสุด ไม่สามารถโต้แย้ง ร้องเรียนหรือทำการเปลี่ยนแปลงคำตัดสินได้ทุกกรณี</li>
+              </ul>
+            </li>
+
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <?php
+  include "src/module/module.php";
+  ?>
+
+  <script src="src/signup/signup.js"></script>
+
+</body>
+
+</html>
